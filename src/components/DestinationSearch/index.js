d371011adc8a237104d8f188bd917dac14f50959
jahnavibarma03 <144089718+jahnavibarma03@users.noreply.github.com>
2024-04-22 22:09:25 +0530

    index.js

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,11 +1,51 @@
-import './index.css'
-import DestinationItem from '../DestinationItem'
+import {Component} from 'react'
 
-const DestinationSearch = props => {
-  const {destinationsList} = props
-  destinationsList.forEach(eachItem => {
-    ;<DestinationItem imgUrl={eachItem.imgUrl} name={eachItem.name} />
-  })
+import DestinationItem from '../DestinationItem'
+import './index.css'
+
+class DestinationSearch extends Component {
+  state = {
+    searchInput: '',
+  }
+
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  render() {
+    const {searchInput} = this.state
+    const {destinationList} = this.props
+    const searchResults = destinationList.filter(eachDestination =>
+      eachDestination.name.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+    return (
+      <div>
+        <div>
+          <h1>Destination Search</h1>
+          <div>
+            <input
+              type="search"
+              placeholder="Search"
+              value={searchInput}
+              onChange={this.onChangeSearchInput}
+            />
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+              alt="search icon"
+            />
+          </div>
+          <ul>
+            {searchResults.map(eachDestination => (
+              <DestinationItem
+                key={eachDestination.id}
+                destinationDetails={eachDestination}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default DestinationSearch
